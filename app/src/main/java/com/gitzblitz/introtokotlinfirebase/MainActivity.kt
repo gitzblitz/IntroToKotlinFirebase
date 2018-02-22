a8d016a208de28b6159eae71cb333157a65226de
George Ng'ethe <gitzblitz@users.noreply.github.com>
2018-02-22 14:20:47 +0200

    Saving objects to Firebase using push

--- app/src/main/java/com/gitzblitz/introtokotlinfirebase/MainActivity.kt
+++ app/src/main/java/com/gitzblitz/introtokotlinfirebase/MainActivity.kt
@@ -16,22 +16,27 @@ class MainActivity : AppCompatActivity() {
     setContentView(R.layout.activity_main)
 
     var firebaseDatabase = FirebaseDatabase.getInstance()
-    var databaseRef = firebaseDatabase.getReference("messages")
-    databaseRef.setValue("Hello There this is live from the Field Office")
+    var databaseRef = firebaseDatabase.getReference("messages").push()
+
+    var employee = Employee("Edwin Bond", 30, "Professor", "1 Main ave")
+//    databaseRef.setValue("Hello There this is live from the Field Office")
+    databaseRef.setValue(employee)
 
 
     //read from database
     databaseRef.addValueEventListener(object : ValueEventListener {
       override fun onCancelled(error: DatabaseError?) {
-        Log.d("ERROR", error!!.message)
+//        Log.d("ERROR", error!!.message)
       }
 
       override fun onDataChange(dataSnapshot: DataSnapshot) {
-        var value = dataSnapshot.value
+        var value = dataSnapshot.value as HashMap<String,Any>
 
-        Log.d("VALUE", value.toString())
-        helloTextView.text = value.toString()
+        Log.d("VALUE", value.get("name").toString())
       }
     })
   }
+
+  data class Employee(var name: String, var age: Int, var position: String, var homeAddress: String)
+
 }
