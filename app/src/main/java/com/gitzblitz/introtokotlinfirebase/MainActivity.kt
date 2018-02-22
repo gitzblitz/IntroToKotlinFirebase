b569676c1d6d23971c522261e62c793833d776c5
George Ng'ethe <gitzblitz@users.noreply.github.com>
2018-02-22 14:00:20 +0200

    Reading from firebase realtime database

--- app/src/main/java/com/gitzblitz/introtokotlinfirebase/MainActivity.kt
+++ app/src/main/java/com/gitzblitz/introtokotlinfirebase/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.gitzblitz.introtokotlinfirebase
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,7 +16,22 @@ class MainActivity : AppCompatActivity() {
     setContentView(R.layout.activity_main)
 
     var firebaseDatabase = FirebaseDatabase.getInstance()
-    var databaseRef = firebaseDatabase.reference
-    databaseRef.setValue("Hello There")
+    var databaseRef = firebaseDatabase.getReference("messages")
+    databaseRef.setValue("Hello There this is live from the Field Office")
+
+
+    //read from database
+    databaseRef.addValueEventListener(object : ValueEventListener {
+      override fun onCancelled(error: DatabaseError?) {
+        Log.d("ERROR", error!!.message)
+      }
+
+      override fun onDataChange(dataSnapshot: DataSnapshot) {
+        var value = dataSnapshot.value
+
+        Log.d("VALUE", value.toString())
+        helloTextView.text = value.toString()
+      }
+    })
   }
 }
